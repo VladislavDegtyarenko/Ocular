390acf0738b7085198e5549f892049a2f1d749c3
Vladislav <vladislavdegtyarenko@gmail.com>
2021-08-12 02:48:20 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# Ocular
+
+
+<img width="180" alt="ocular" src="https://ocular.com.ua/wp-content/uploads/2021/03/logo.svg">
+
+Front-end development for "Ocular" - large-scale medical center for adult and pediatric ophthalmology.
+
+
+
+[Click here](https://vladislavdegtyarenko.github.io/Ocular/html/dist/) to view the project
+
+### Requirements for the project
+- **HTML** layout, W3C validated
+- responsive **CSS** layout across all devices
+- **Javascript** programming (burger menu, sliders, modal windows, form validation)
+- page speed load optimization
+
+### Design
+Design was developed using Figma. [Click here](https://www.figma.com/file/KvCvSEBDq2FtCCQIU59UwA/Ocular-LP-Copy) to see the mockup.
+<img width="1336" alt="Screenshot 2021-08-11 at 17 43 19" src="https://user-images.githubusercontent.com/62521930/129050374-d12665bc-3e41-4e6f-8ee2-421814022853.png">
+
+
+
+### Technologies used
+<p align="left">
+  <a href="https://www.w3.org/html/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg" alt="html5" width="40" height="40"/> </a> <a href="https://www.w3schools.com/css/" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg" alt="css3" width="40" height="40"/> </a> <a href="https://sass-lang.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/sass/sass-original.svg" alt="sass" width="40" height="40"/> </a> <a href="https://gulpjs.com" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/gulp/gulp-plain.svg" alt="gulp" width="40" height="40"/> </a> <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"> <img src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg" alt="javascript" width="40" height="40"/> </a> 
+</p>
+
+
+### Usage
+
+1. install node modules
+```bash
+npm i
+```
+
+2. run gulp
+```bash
+gulp
+```
+
+#
+
+Thanks for paying attention to this repository. You can find more interesting projects in my [Github profile]().
+
+*Best regards,*
+
+*Vladislav*
---
+++ html/src/js/script.js
@@ -0,0 +1,440 @@
+document.addEventListener('DOMContentLoaded', function() {
+	MicroModal.init({
+		awaitCloseAnimation: true,
+		awaitOpenAnimation: true,
+		disableFocus: true,
+		onShow: modal => bodyAddScrollLock(),
+		onClose: modal => bodyRemoveScrollLock()
+	});
+
+	new Splide( '#careSlider', {
+		classes: {
+			arrows: 'splide__arrows your-class-arrows',
+			arrow : 'splide__arrow your-class-arrow',
+			prev  : 'splide__arrow-prev your-class-prev',
+			next  : 'splide__arrow-next your-class-next',
+		},
+		pagination: false,
+		type: 'loop',
+		perPage: 1,
+		perMove: 1,
+		breakpoints: {
+			767: {
+				pagination: true,
+			}
+		}
+	} ).mount();
+
+	const servicesSliders = document.querySelectorAll( '.services__slider' );
+
+	for ( let i = 0, len = servicesSliders.length; i < len; i++ ) {
+		new Splide( servicesSliders[i], {
+			classes: {
+				arrows: 'splide__arrows your-class-arrows',
+				arrow : 'splide__arrow your-class-arrow',
+				prev  : 'splide__arrow-prev your-class-prev',
+				next  : 'splide__arrow-next your-class-next',
+			},
+			pagination: true,
+			perPage: 3,
+			perMove: 1,
+			breakpoints: {
+				1199: {
+					perPage: 2,
+				},
+				767: {
+					perPage: 1,
+				}
+			}
+		} ).mount();
+	}
+
+	new Splide( '#doctorsSlider', {
+		classes: {
+			arrows: 'splide__arrows your-class-arrows',
+			arrow : 'splide__arrow your-class-arrow',
+			prev  : 'splide__arrow-prev your-class-prev',
+			next  : 'splide__arrow-next your-class-next',
+		},
+		perPage: 4,
+		perMove: 1,
+		type: 'slider',
+		pagination: false,
+		breakpoints: {
+			1199: {
+				perPage: 3,
+			},
+			991: {
+				perPage: 2
+			},
+			575: {
+				perPage: 2,
+				pagination: true,
+			}
+		}
+	} ).mount();
+	
+	new Splide( '#certificatesSlider', {
+		classes: {
+			arrows: 'splide__arrows your-class-arrows',
+			arrow : 'splide__arrow your-class-arrow',
+			prev  : 'splide__arrow-prev your-class-prev',
+			next  : 'splide__arrow-next your-class-next',
+		},
+		perPage: 3,
+		perMove: 1,
+		pagination: false,
+		gap: '40px',
+		breakpoints: {
+			991: {
+				gap: '20px'
+			},
+			767: {
+				perPage: 2,
+				gap: '10px',
+				pagination: true
+			},
+			575: {
+				perPage: 2,
+				pagination: true
+			}
+		}
+	} ).mount();
+	
+	new Splide( '#reviewsSlider', {
+		classes: {
+			arrows: 'splide__arrows your-class-arrows',
+			arrow : 'splide__arrow your-class-arrow',
+			prev  : 'splide__arrow-prev your-class-prev',
+			next  : 'splide__arrow-next your-class-next',
+		},
+		perPage: 2,
+		perMove: 1,
+		type: 'loop',
+		pagination: true,
+		gap: '40px',
+		breakpoints: {
+			991: {
+				perPage: 1
+			}
+		}
+	} ).mount();
+	
+	new Splide( '#equipmentSlider', {
+		classes: {
+			arrows: 'splide__arrows your-class-arrows',
+			arrow : 'splide__arrow your-class-arrow',
+			prev  : 'splide__arrow-prev your-class-prev',
+			next  : 'splide__arrow-next your-class-next',
+		},
+		type: 'loop',
+		pagination: true,
+	} ).mount();
+
+
+	/* SERVICES MENU / TABS */
+	const servicesMenu = document.querySelector('.services__menu'),
+			servicesTabs = document.querySelectorAll('.services__tab'),
+			servicesSelected = document.querySelector('.services__selected');
+
+	servicesTabs.forEach(tab => {
+		tab.addEventListener('click', function(event) {
+			servicesTabActiveClassSet(tab);
+
+			servicesSelectedTextInit();
+
+			servicesSwitchSlides(event);
+		})
+	})
+	servicesSelected.addEventListener('click', toggleServiceMenu)
+
+	function servicesTabActiveClassSet(tab) {
+		servicesTabs.forEach (tab => {
+			tab.classList.remove('services__tab-active')
+		})
+		tab.classList.add('services__tab-active');
+	}
+
+	function servicesSwitchSlides(event) {
+		let servicesActiveTab = event.target.dataset.tabCategory;
+
+		servicesSliders.forEach(slider => {
+			slider.classList.remove('services__slider-active')
+
+			if (slider.dataset.sliderCategory === servicesActiveTab) {
+				slider.classList.add('services__slider-active');
+				toggleServiceMenu();
+			}
+		})
+	}
+
+	function servicesSelectedTextInit(tab) {
+		let servicesTabActive = document.querySelector('.services__tab-active');
+		let servicesTabActiveText = servicesTabActive.innerHTML;
+
+		servicesSelected.innerHTML = servicesTabActiveText;
+	}
+
+	function toggleServiceMenu() {
+		servicesMenu.classList.toggle('services__menu-opened')
+	}
+	servicesSelectedTextInit();
+
+	// detect click outside servicesMenu
+	document.addEventListener('click', function(event) {
+		const isClickInside = servicesMenu.contains(event.target);
+		let isMenuOpened = document.querySelector('.services__menu-opened');
+	 
+		if (!isClickInside && isMenuOpened) {
+		  //the click was outside the specifiedElement, do something
+			toggleServiceMenu()
+		}
+	});
+
+
+	/* burger menu */
+   const burgerMenuSticks = document.querySelectorAll('.stick');
+	const mobileMenu = document.querySelector('.header__nav');
+	const mobileMenuCurtain = document.querySelector('.nav__curtain');
+	const toggleMobileMenuButtons = document.querySelectorAll('.jsToggleMobileMenu');
+	const openMobileMenuButtons = document.querySelectorAll('.jsOpenMobileMenu');
+	const closeMobileMenuButtons = document.querySelectorAll('.jsCloseMobileMenu');
+
+	toggleMobileMenuButtons.forEach(button => {
+		button.addEventListener('click', () => {
+			toggleMobileMenu();
+
+		})
+	})
+
+	openMobileMenuButtons.forEach(button => {
+		button.addEventListener('click', () => {
+			openMobileMenu();
+		})
+	})
+
+	closeMobileMenuButtons.forEach(button => {
+		button.addEventListener('click', () => {
+			closeMobileMenu();
+		})
+	})
+
+	const bodyAddScrollLockButtons = document.querySelectorAll('.jsBodyAddScrollLock');
+	const bodyRemoveScrollLockButtons = document.querySelectorAll('.jsBodyRemoveScrollLock');
+
+	bodyAddScrollLockButtons.forEach(button => {
+		button.addEventListener('click', () => {
+			bodyAddScrollLock()
+		})
+	})
+
+	bodyRemoveScrollLockButtons.forEach(button => {
+		button.addEventListener('click', () => {
+			bodyRemoveScrollLock()
+		})
+	})
+
+	function toggleMobileMenu() {
+		burgerMenuSticks.forEach(stick => {
+         stick.classList.toggle('close');
+         stick.classList.toggle('open');
+		})
+		mobileMenu.classList.toggle('header__nav-closed');
+		mobileMenu.classList.toggle('header__nav-opened');
+
+		mobileMenuCurtain.classList.toggle('curtain-down');
+		mobileMenuCurtain.classList.toggle('curtain-up');
+
+		document.body.classList.toggle('noScroll');
+	}
+
+	function openMobileMenu() {
+		burgerMenuSticks.forEach(stick => {
+         stick.classList.remove('close');
+         stick.classList.add('open');
+		})
+		mobileMenu.classList.remove('header__nav-closed');
+		mobileMenu.classList.add('header__nav-opened');
+
+		mobileMenuCurtain.classList.remove('curtain-down');
+		mobileMenuCurtain.classList.add('curtain-up');
+		
+		bodyAddScrollLock();
+	}
+
+	function closeMobileMenu() {
+		burgerMenuSticks.forEach(stick => {
+         stick.classList.remove('open');
+         stick.classList.add('close');
+		})
+		mobileMenu.classList.remove('header__nav-opened');
+		mobileMenu.classList.add('header__nav-closed');
+
+		mobileMenuCurtain.classList.remove('curtain-up');
+		mobileMenuCurtain.classList.add('curtain-down');
+		
+		bodyRemoveScrollLock();
+	}
+
+	function bodyAddScrollLock() {
+		document.body.classList.add('noScroll');
+	}
+
+	function bodyRemoveScrollLock() {
+		document.body.classList.remove('noScroll');
+	}
+
+
+
+	const certCards = document.querySelectorAll('.certificates__card');
+
+	certCards.forEach(card => {
+		card.addEventListener('click', function(event) {
+			if (event.target.classList.contains('certificates__card-click')) {
+				event.target.classList.remove('certificates__card-click');
+			} else {
+				certCards.forEach(card => {
+					card.classList.remove('certificates__card-click');
+				})
+	
+				card.classList.add('certificates__card-click');
+			}
+		})
+	})
+
+
+	/* handling scroll position */
+	const header = document.querySelector('.header');
+
+	if (
+		"IntersectionObserver" in window &&
+		"IntersectionObserverEntry" in window &&
+		"intersectionRatio" in window.IntersectionObserverEntry.prototype
+	 ) {
+	 let observer = new IntersectionObserver(entries => {
+		if (entries[0].boundingClientRect.y < 0) {
+			header.classList.add("header-hidden");
+		} else {
+			header.classList.remove("header-hidden");
+		}
+	 });
+	 observer.observe(document.querySelector("#pixel-anchor"));
+	 }
+
+
+
+	/* form validation */
+
+  // inputMask
+  let inputs = document.querySelectorAll('input[type="tel"]');
+  let im = new Inputmask('+38099-999-99-99');
+  im.mask(inputs);
+
+  // validate
+  	function validateForms(selector, modalWindow) {
+		new window.JustValidate(selector, {
+			colorWrong: '#FF3D3D',
+
+			rules: {
+				name: {required: true},
+				tel: {required: true, strength: { custom: '^.[0-9]{5}-?[0-9]{3}-?[0-9]{2}-?[0-9]{2}' }},
+				comment: {required: true, minLength: 5}
+			},
+			messages: {
+				name : {
+					required: `Це поле обов'язкове для заповнення`
+				},
+				tel: {
+					required: `Це поле обов'язкове для заповнення`,
+					strength: `Заповніть, будь ласка, це поле`
+				},
+				comment: {
+					required: `Це поле обов'язкове для заповнення`, 
+					minLength: `Поле має містити не менше :value символів.`
+				}
+			},
+			submitHandler: function(form, values, ajax) {
+				let formData = new FormData(form);
+				let errorDiv = form.getElementsByClassName('form-error-message')[0];
+				let loadingIndicator = form.getElementsByClassName('form-loading-indicator')[0];
+
+				errorDiv.style.display = 'none';
+				errorDiv.innerHTML = '';
+				loadingIndicator.style.display = 'block';
+
+				fetch(myAjax.url, {
+					method: "POST",
+					body: formData
+				})
+				.then(function(response) {
+					return response.json();
+				}).then(function (data) {
+					if (data.error) {
+						errorDiv.style.display = 'block';
+						errorDiv.innerHTML = data.error;
+					} else {
+						form.reset();
+						MicroModal.close(modalWindow);
+						MicroModal.show('thankYou');
+					}
+				}).finally(function() {
+					loadingIndicator.style.display = 'none';
+				});
+			}
+		});
+	}
+	  
+	validateForms('#formMain', 'modalMainForm');
+	validateForms('#formCall', 'modalMainCall');
+
+	const serviceCardsForms = document.querySelectorAll('.formServices');
+
+	serviceCardsForms.forEach(form => {
+		let formId = "#" + form.id;
+		let formModalWindowId = form.closest('.modal__service').id;
+		validateForms(formId, formModalWindowId);
+	})
+
+	// two cards centered
+	let intViewportWidth = window.innerWidth;
+
+	function cardsCentered() {
+		servicesSliders.forEach((slider) => {
+			if ((intViewportWidth > 1199) && (slider.querySelector('.splide__list').childElementCount == 2)) {
+				slider.querySelector('.splide__track').style.display = 'flex';
+				slider.querySelector('.splide__track').style.justifyContent = 'center';
+			} else if ((intViewportWidth > 767) && (slider.querySelector('.splide__list').childElementCount == 1)) {
+				slider.querySelector('.splide__track').style.display = 'flex';
+				slider.querySelector('.splide__track').style.justifyContent = 'center';
+			}
+			else {
+				slider.querySelector('.splide__track').style.display = '';
+				slider.querySelector('.splide__track').style.justifyContent = '';
+			}
+		})
+	}
+	cardsCentered();
+
+
+
+	/* equipment slider arrows vertical align */
+	const equipmentSlider = document.querySelector('#equipmentSlider');
+	const equipmentSliderPhoto = equipmentSlider.querySelector('.equipment__card_photo');
+	const equipmentSliderArrowPrev = equipmentSlider.querySelector('.splide__arrow-prev');
+	const equipmentSliderArrowNext = equipmentSlider.querySelector('.splide__arrow-next');
+
+	function equipmentSliderArrowsAlign() {
+		equipmentSliderArrowPrev.style.bottom = `${equipmentSliderPhoto.offsetHeight / 2}px`;
+		equipmentSliderArrowNext.style.bottom = `${equipmentSliderPhoto.offsetHeight / 2}px`;
+	}
+	equipmentSliderArrowsAlign();
+
+	/* resizing events */
+	window.addEventListener('resize', function() {
+		intViewportWidth = window.innerWidth;
+		cardsCentered();
+		equipmentSliderArrowsAlign();
+	})
+}) /* DOMContentLoaded */
+
+	
